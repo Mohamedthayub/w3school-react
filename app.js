e4dd5fc763a6bdf628b5a9b4b212caee8559def4
buyath <buyaththayub@gmail.com>
2025-06-17 03:10:21 +0000

    initiated the project

---
+++ app.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Heading = () => {
+    return (
+        <div>
+            <h1>Asalamu alaikum</h1>
+        </div>
+    )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Heading/>);
