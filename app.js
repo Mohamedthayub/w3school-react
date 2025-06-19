f5f80548dc26805cf3234e8a240986d8c66a2ec2
buyath <buyaththayub@gmail.com>
2025-06-19 14:20:19 +0000

    iterate list with map done

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Greater = () => {
-    return <h1 style={{color : "green"}}>you are eligible to put vote</h1>
-}
-const Lesser = () => {
-    return <h1 style={{color : "red"}}>You are not eligible</h1>
+
+function CreateList(props){
+    return <li>{props.brand}</li>
 }
 
-function CheckAge(props){
-    const isage =  props.isage;
-    return <div>  {
-        isage > 18 ? <Greater/> :  <Lesser/>
-    }
-    </div>
-}
+
 
 const App = () => {
+    const cars = ["Bmw","Audi","Suzuki"];
     return <div>
-        <CheckAge isage = {18}></CheckAge>
+    <ul>
+        {cars.map((item) => <CreateList brand = {item}/>)}        
+
+    </ul>
     </div>
 }
 
---
+++ test.js
@@ -0,0 +1,4 @@
+const number = [1,2,3,4,5,6];
+number.map((num) => {
+    console.log(num * 2);
+})
