402b9326fe64a8eae2f9366a4a878f0d5d7de884
buyath <buyaththayub@gmail.com>
2025-07-01 01:14:49 +0000

    useMemo hook deos perfomance think here when the component changing  the cubeNum function is not executing .

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-import React, { useState,useEffect, useRef } from "react";
+import React, { useState,useEffect, useRef,useMemo } from "react";
 import ReactDOM from "react-dom/client";
 
 
 function Calculator(){
     const [number , setNumber] = useState(0);
-    // const [count , setCounter ] = useState(0);
+    const [count , setCounter ] = useState(0);
     
 
     function cubeNum (num){
         console.log("calculation Done ...");
         return Math.pow(num,3);
     }
-    const result = cubeNum(number);
+    const result = useMemo(() => cubeNum(number),[number]);
 
     return (
-        <div>
-            <input type="number" value={number} onChange={(e) => {setNumber(Number(e.target.value))}}></input>
-            <h1>Cube of the Number : {result}</h1>
+        <div className="main">
+            <div>
+              <input type="number" value={number} onChange={(e) => {setNumber(Number(e.target.value))}}></input>
+              <h1>Cube of the Number : {result}</h1>
+              <button onClick={() => setCounter(count + 1)}>Counter++</button>
+              <h1>Counter : {count}</h1>
+           </div>
         </div>
     )
 }
