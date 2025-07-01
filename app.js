6b6ac292c047574cba3841844f93e7a90a6f2684
buyath <buyaththayub@gmail.com>
2025-07-01 00:10:29 +0000

    this is how we  have to select the element by useRef

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-import React, { useState,useEffect, use } from "react";
+import React, { useState,useEffect, useRef } from "react";
 import ReactDOM from "react-dom/client";
 
 
-
 function Counter(){
-    const [count,setCount ] = useState(0);
-    useEffect(() => {
-        setTimeout(() => {
-            setCount(count + 1);
-        },1000)   // useEffect runs on every render. That means that when the count changes, a render happens, which then triggers another effect
-        // when the component changing it will useEffect will re-render the page  
+    const inputElement = useRef();
+    
+    const btnClicked = () => {
+        console.log(inputElement.current);
+        inputElement.current.style.background = "red";
+    }
 
-
-    })
     return (
-        <div className="main">
-            <div className="counter-container">
-                <h1> I have rendered  {count} times </h1>
-
-            </div>
+        <div>
+            <input type="text" ref={inputElement}></input>
+            <button onClick={btnClicked}>Click</button>
         </div>
     )
 }
