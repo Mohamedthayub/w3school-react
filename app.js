17700d0efeb322dd6d9f89119bbe099a4d428939
buyath <buyaththayub@gmail.com>
2025-07-03 15:48:08 +0000

    i have done simple counter by react styling for tailwind css

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 import React, { useState,useEffect,useRef} from "react";
 import ReactDOM from "react-dom/client";
-const names = [{name : "thayub",id : 1},{name : "mohamed",id: 2},{name : "bilal" , id: 3}];
-const CreateHeading = (props) => {
+
+
+function  Counter() {
+    const [count, setCounter] = useState(0);
     return (
         <div>
-            <h1>{props.name}</h1>
+            <div className="flex justify-center items-center mt-92">
+                <div>
+                    <h1 className="text-center text-3xl"> Count is : {count}</h1>
+            <div className="mt-4">
+                <button onClick={() => {
+                    setCounter(count + 1)
+                }} className="p-4 focus:outline-none text-white bg-green-700 hover:bg-green-800  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Increament</button>
+                <button onClick={() => {
+                    setCounter(0)
+                }} className="focus:outline-none text-white bg-red-700 hover:bg-red-800   font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Reset</button>
+                <button
+                onClick={() => {
+                    setCounter(count - 1)
+                }} className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Decreament</button>
+            </div>
+                </div>
+            </div>
         </div>
     )
 }
 const App = () => {
     return (
-        <div className="main">
-           {
-            names.map((item) => (
-                <CreateHeading  key = {item.id}name = {item.name}/>
-            ))
-           }
+        <div className="">
+            <Counter/>
        </div>
     )
 
