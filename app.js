db9bc829ba74dc1ec05243d3f57206ed5fb4e09e
buyath <buyaththayub@gmail.com>
2025-07-05 18:32:50 +0000

    this is how we add data current data using useRef hook

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ import Navbar from "./components/Navbar.js";
 import ReactDOM from "react-dom/client";
 
 const App =  () => {
-    const [name, setName] = useState("");
+    const [data, setData] = useState([]);
     const input = useRef(null);
+
     return (
         <div>
             {/* <Navbar cnt = {name} fn ={setName}/> */}
-            <input ref={input} type="text"></input>
-            <button onClick={() => {console.log(input.current.value)}}>Submit</button>
+            <input ref={input} type="text"></input> {/* we select the html element using  useref  ref keyword use to conect the html element*/}
+            <button onClick={() => {setData([...data,input.current.value])}}>Submit</button> {/* we send the data current data to  the array when the button click*/}
+            {data.map((item,index) => { return <h2 key={index}>{item}</h2>})} {/*we iterate the value using map function but here we should index as key when we iterate the array data */}
         </div>
     )
 }
---
+++ test.js
@@ -0,0 +1,5 @@
+let number = [1,2,3,4,5,6,7];
+
+number.forEach((num,index) => {
+    console.log(`this is value ${num} this is index ${index}`);
+})
