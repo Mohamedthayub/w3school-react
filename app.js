782264b077b1eb54d35c39b6c6e8034d4f426bad
buyath <buyaththayub@gmail.com>
2025-07-05 19:47:38 +0000

    today react practice done

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const App =  () => {
         <div>
             {/* <Navbar cnt = {name} fn ={setName}/> */}
             <input ref={input} type="text"></input> {/* we select the html element using  useref  ref keyword use to conect the html element*/}
-            <button onClick={() => {setData([...data,input.current.value])}}>Submit</button> {/* we send the data current data to  the array when the button click*/}
+            <button onClick={() => {setData([...data,input.current.value])}}>Submit</button>
+             {/* we send the data current data to  the array when the button click*/}
             {data.map((item,index) => { return <h2 key={index}>{item}</h2>})} {/*we iterate the value using map function but here we should index as key when we iterate the array data */}
         </div>
     )
