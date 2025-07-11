811b4b082649eb6dae935d7ddb20352ab1df3a16
buyath <buyaththayub@gmail.com>
2025-07-11 16:10:37 +0000

    so far revised i concepts

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-import React, {useState,useRef} from "react";
-import Navbar from "./components/Navbar.js";
+import React, {useState,useRef, useEffect} from "react";
+// import Navbar from "./components/Navbar.js";
 import ReactDOM from "react-dom/client";
 
-const App =  () => {
-    const [data, setData] = useState([]);
-    const input = useRef(null);
 
+const App = () => {
+    const [data,setData] = useState([]);
+    const inputRef = useRef(null);
     return (
         <div>
-            {/* <Navbar cnt = {name} fn ={setName}/> */}
-            <input ref={input} type="text"></input> {/* we select the html element using  useref  ref keyword use to conect the html element*/}
-            <button onClick={() => {setData([...data,input.current.value])}}>Submit</button>
-             {/* we send the data current data to  the array when the button click*/}
-            {data.map((item,index) => { return <h2 key={index}>{item}</h2>})} {/*we iterate the value using map function but here we should index as key when we iterate the array data */}
+            <div>
+                <input ref={inputRef} type="text"></input>
+                <button onClick={() => { setData([...data,inputRef.current.value])}}>Submit</button>
+                {data.map((item,index) => { return <h1 key={index}>{item}</h1>})}
+            </div>
         </div>
     )
 }
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App/>);
  
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,9 @@
-const Navbar = ({cnt,fn}) => {
-    return (
+const Navbar = ({cnt,setcnt}) => {
+    return  (
         <div>
             <h1>{cnt}</h1>
-            <button onClick={() => fn("thayub")}>add</button>
+            <button onClick={() => {setcnt(() => cnt + 1)}} >Add</button>
         </div>
     )
 }
-
-export default Navbar
+export default Navbar;
