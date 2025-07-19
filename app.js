0e13905475ee8ebec26be7bcb383003b52bfc316
buyath <buyaththayub@gmail.com>
2025-07-19 16:16:26 +0000

    i rendered image by image link

--- app.js
+++ app.js
@@ -1,33 +1,8 @@
-import React, {useState,useRef, useEffect} from "react";
-// import Navbar from "./components/Navbar.js";
+import React from "react";
 import ReactDOM from "react-dom/client";
+import {Heading } from "./src/Heading";
 
 
-const Counter = () => {
-    const [count, setCounter] = useState(0);
-    return (
-        <div>
-            <h1>{count}</h1>
-            <div>
-                <button className="increament" onClick={() => {setCounter(count + 1)}}>increament</button>
-            </div>
-            <div>
-                <button className="increament" onClick={() => {setCounter(0)}}>Reset</button>
-            </div>
-            <div>
-                <button className="increament" onClick={() => {setCounter(count -  1)}}>decreament</button>
-            </div>
-        </div>
-    )
-}
 
-const App = () => {
-    return (
-        <div>
-            <Counter/>
-        </div>
-    )
-}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App/>);
- 
+root.render(<Heading/>)
---
+++ src/Heading.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import car from "./car.jpg"; 
+
+export const Heading = () => {
+  return (
+    <div> 
+      <img src= "https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" alt="Car" className="img"/>
+      <h1>This is in jsx</h1>
+    </div>
+  );
+};
